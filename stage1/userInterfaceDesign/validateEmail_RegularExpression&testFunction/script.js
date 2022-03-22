eaa68620cc22286baffb852af5a0faeabcd5f210
SAKSHAMVIPER <sakshamsinghrkt387@gmail.com>
2022-03-22 14:20:40 +0530

    validate

---
+++ stage1/userInterfaceDesign/validateEmail_RegularExpression&testFunction/script.js
@@ -0,0 +1,10 @@
+function validateEmail(email) {
+ var emailCheck= /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+if(emailCheck.test(email)){
+    return "Valid email address!";
+    
+}
+else{
+    return "Invalid email address!";
+}
+}
